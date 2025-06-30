5992d7863106b2e8b62de82790329b813ea6a495
atefeh <ativalimohammadi11@gmail.com>
2025-06-30 11:29:36 +0330

    fix margins and paddings

--- src/components/Sidebar/SidebarNavLink.tsx
+++ src/components/Sidebar/SidebarNavLink.tsx
@@ -16,17 +16,26 @@ const SidebarNavLink: React.FC<SidebarNavLinkProps> = (props) => {
   const isActive = href === pathname;
 
   const classNames = `
-    flex justify-start items-center h-[35px] w-full gap-x-2 rounded-[2px] px-1.5 py-[9px] transition-all 
+    flex justify-start items-center w-full gap-x-2 rounded-[2px] p-2.5 transition-all 
     duration-300
     hover:bg-nav-link-active-bg
-    ${isActive && "bg-nav-link-active-bg border-r-[4px] border-solid border-white/20"}`;
+    ${
+      isActive &&
+      "bg-nav-link-active-bg border-r-[4px] border-solid border-white/20"
+    }`;
 
   return (
     <Link href={href} className={classNames}>
       <div className="relative h-4 w-4">
         <Image src={iconSrc} alt={title} fill />
       </div>
-      <p className={`text-left text-profile-card-text text-base-500 ${isActive ? "text-white" : ""}`}>{title}</p>
+      <p
+        className={`text-left text-profile-card-text text-base-500 ${
+          isActive ? "text-white" : ""
+        }`}
+      >
+        {title}
+      </p>
     </Link>
   );
 };
--- src/modules/Layout/Sidebar.tsx
+++ src/modules/Layout/Sidebar.tsx
@@ -4,30 +4,38 @@ import SidebarNavLink from "@/components/Sidebar/SidebarNavLink";
 import { APP_NAVIGATION, APP_ROUTES } from "@/constants/SidebarRoutes";
 import BrandLogo from "@/components/Sidebar/BrandLogo";
 
-
 const Sidebar = () => {
   return (
-    <aside className="
+    <aside
+      className="
         h-screen w-[var(--app-sidebar-width)] border-r border-solid border-white/10 bg-sidebar-bg
         pt-6 pl-5 pr-6 fixed left-0 top-0
-    ">
+    "
+    >
       <header className="flex h-[var(--app-navbar-height)] items-center justify-start pl-[7px]">
         <Link href={APP_ROUTES.dashboard}>
-          <BrandLogo/>
+          <BrandLogo />
         </Link>
       </header>
-      <nav className="flex flex-col gap-y-[38px] pt-[55px]">
+      <nav className="flex flex-col gap-y-8 pt-16">
         {APP_NAVIGATION.map((navItem) => {
           if (navItem.children) {
             return (
-                <div key={navItem.title}>
-                    <p className="text-left text-nav-sub-menu-heading-text text-sm">{navItem.title}</p>
-                    {navItem.children.map(childrenItem =>{
-                        return(
-                            <SidebarNavLink key={childrenItem.title} {...childrenItem}/>
-                        )
-                    })}
+              <div key={navItem.title}>
+                <p className="text-left text-nav-sub-menu-heading-text text-sm mb-3">
+                  {navItem.title}
+                </p>
+                <div className="flex flex-col gap-[18px]">
+                  {navItem.children.map((childrenItem) => {
+                    return (
+                      <SidebarNavLink
+                        key={childrenItem.title}
+                        {...childrenItem}
+                      />
+                    );
+                  })}
                 </div>
+              </div>
             );
           } else {
             return <SidebarNavLink key={navItem.title} {...navItem} />;
