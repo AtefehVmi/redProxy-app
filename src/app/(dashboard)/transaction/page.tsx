e83175959be06616647d08df034725d724769ea3
atefeh <ativalimohammadi11@gmail.com>
2025-07-05 12:20:45 +0330

    fix transactions table

--- src/app/(dashboard)/transaction/page.tsx
+++ src/app/(dashboard)/transaction/page.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
+import React from "react";
 import UserInvoices from "@/modules/Transactions/UserInvoices";
 
 const Page = () => {
-    return (
-        <div className={"w-full h-full flex flex-col gap-[33px]"}>
-            <div className="flex flex-col items-start gap-1.5">
-                <p className="text-xl font-semibold text-white">
-                    Your transactions
-                </p>
-                <p className="text-sm font-medium text-nav-sub-menu-heading-text">
-                    Overview of all you transactions
-                </p>
-            </div>
-            <UserInvoices />
-        </div>
-    );
+  return (
+    <div className={"w-full h-full flex flex-col gap-[33px] pb-8"}>
+      <div className="flex flex-col items-start gap-1.5">
+        <p className="text-xl font-semibold text-white">Your transactions</p>
+        <p className="text-sm font-medium text-nav-sub-menu-heading-text">
+          Overview of all you transactions
+        </p>
+      </div>
+      <UserInvoices />
+    </div>
+  );
 };
 
 export default Page;
--- src/modules/Transactions/UserInvoices.tsx
+++ src/modules/Transactions/UserInvoices.tsx
@@ -1,150 +1,148 @@
-'use client'
-import React from 'react';
+"use client";
+import React from "react";
 import CustomCard from "@/components/CustomCard/customCard";
 import SelectWithCustomCard from "@/components/CustomSelect/SelectWithCustomCard";
 import Table from "@/components/Table/Table";
-import {InvoiceColumns, InvoiceDataType} from "@/constants/TableColumns";
-
+import { InvoiceColumns, InvoiceDataType } from "@/constants/TableColumns";
+import cn from "@/utils/cn";
 
 const INVOICE_DATA: Array<InvoiceDataType> = [
-    {
-        date: "08/05/2024",
-        invoice: "#982ZK-001",
-        order: "100 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "PAID",
-        download: "https://www.google.com"
-    },
-    {
-        date: "09/05/2024",
-        invoice: "#982ZK-002",
-        order: "10 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "PAID",
-        download: "https://www.google.com"
-    },
-    {
-        date: "10/05/2024",
-        invoice: "#982ZK-003",
-        order: "50 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "PAID",
-        download: "https://www.google.com"
-    },
-    {
-        date: "11/05/2024",
-        invoice: "#982ZK-004",
-        order: "20 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "FAILED",
-        download: "https://www.google.com"
-    },
-    {
-        date: "10/05/2024",
-        invoice: "#982ZK-003",
-        order: "50 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "PAID",
-        download: "https://www.google.com"
-    },
-    {
-        date: "10/05/2024",
-        invoice: "#982ZK-003",
-        order: "50 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "PAID",
-        download: "https://www.google.com"
-    },
-    {
-        date: "10/05/2024",
-        invoice: "#982ZK-003",
-        order: "50 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "PAID",
-        download: "https://www.google.com"
-    },
-    {
-        date: "10/05/2024",
-        invoice: "#982ZK-003",
-        order: "50 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "PAID",
-        download: "https://www.google.com"
-    },
-    {
-        date: "10/05/2024",
-        invoice: "#982ZK-003",
-        order: "50 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "PAID",
-        download: "https://www.google.com"
-    },
-    {
-        date: "10/05/2024",
-        invoice: "#982ZK-003",
-        order: "50 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "PAID",
-        download: "https://www.google.com"
-    },
-    {
-        date: "10/05/2024",
-        invoice: "#982ZK-003",
-        order: "50 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "PAID",
-        download: "https://www.google.com"
-    },
-    {
-        date: "10/05/2024",
-        invoice: "#982ZK-003",
-        order: "50 GB Residential",
-        amount: "$ 300.00",
-        paymentMethod: "Mastercard",
-        status: "PAID",
-        download: "https://www.google.com"
-    },
-]
-
+  {
+    date: "08/05/2024",
+    invoice: "#982ZK-001",
+    order: "100 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "PAID",
+    download: "https://www.google.com",
+  },
+  {
+    date: "09/05/2024",
+    invoice: "#982ZK-002",
+    order: "10 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "PAID",
+    download: "https://www.google.com",
+  },
+  {
+    date: "10/05/2024",
+    invoice: "#982ZK-003",
+    order: "50 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "PAID",
+    download: "https://www.google.com",
+  },
+  {
+    date: "11/05/2024",
+    invoice: "#982ZK-004",
+    order: "20 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "FAILED",
+    download: "https://www.google.com",
+  },
+  {
+    date: "10/05/2024",
+    invoice: "#982ZK-003",
+    order: "50 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "PAID",
+    download: "https://www.google.com",
+  },
+  {
+    date: "10/05/2024",
+    invoice: "#982ZK-003",
+    order: "50 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "PAID",
+    download: "https://www.google.com",
+  },
+  {
+    date: "10/05/2024",
+    invoice: "#982ZK-003",
+    order: "50 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "PAID",
+    download: "https://www.google.com",
+  },
+  {
+    date: "10/05/2024",
+    invoice: "#982ZK-003",
+    order: "50 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "PAID",
+    download: "https://www.google.com",
+  },
+  {
+    date: "10/05/2024",
+    invoice: "#982ZK-003",
+    order: "50 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "PAID",
+    download: "https://www.google.com",
+  },
+  {
+    date: "10/05/2024",
+    invoice: "#982ZK-003",
+    order: "50 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "PAID",
+    download: "https://www.google.com",
+  },
+  {
+    date: "10/05/2024",
+    invoice: "#982ZK-003",
+    order: "50 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "PAID",
+    download: "https://www.google.com",
+  },
+  {
+    date: "10/05/2024",
+    invoice: "#982ZK-003",
+    order: "50 GB Residential",
+    amount: "$ 300.00",
+    paymentMethod: "Mastercard",
+    status: "PAID",
+    download: "https://www.google.com",
+  },
+];
 
 const UserInvoices = () => {
-    function onFilterChange() {
+  function onFilterChange() {}
 
-    }
-
-    return (
-        <CustomCard
-            borderRadius={"rounded"}
-            borderClassName={"w-full h-[700px] p-[1.75px]"}
-            containerClassName={"px-4 pt-5 pb-4 flex flex-col items-center gap-[18px] overflow-auto"}
-        >
-            <div className="w-full flex justify-between items-center">
-                <p className="text-base font-semibold text-white">Invoices</p>
-                <SelectWithCustomCard
-                    options={[
-                        {label: "Last 12 months", value: "year"},
-                        {label: "Last 30 days", value: "month"},
-                        {label: "Last 7 days", value: "week"},
-                    ]}
-                    defaultValue={"week"}
-                    onChange={onFilterChange}
-                    className="w-[119px] h-26px"
-                />
-            </div>
-            <Table columns={InvoiceColumns} data={INVOICE_DATA}/>
-        </CustomCard>
-    );
+  return (
+    <div
+      className={cn(
+        "p-[1.75px] px-4 pt-5 pb-4 flex flex-col items-center gap-[18px] overflow-auto",
+        "border border-darkmode-100 bg-darkmode-200 rounded w-full h-[700px]"
+      )}
+    >
+      <div className="w-full flex justify-between items-center">
+        <p className="text-base font-semibold text-white">Invoices</p>
+        <SelectWithCustomCard
+          options={[
+            { label: "Last 12 months", value: "year" },
+            { label: "Last 30 days", value: "month" },
+            { label: "Last 7 days", value: "week" },
+          ]}
+          defaultValue={"week"}
+          onChange={onFilterChange}
+          className="w-[119px] h-26px"
+        />
+      </div>
+      <Table columns={InvoiceColumns} data={INVOICE_DATA} />
+    </div>
+  );
 };
 
 export default UserInvoices;
